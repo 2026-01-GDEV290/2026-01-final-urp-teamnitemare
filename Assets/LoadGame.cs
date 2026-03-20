cf9cf815a1fccdd3fbd525dc12a51b3009fa6dfe
SadXLou <g00318629@stu.raritanval.edu>
2026-03-20 13:41:53 -0400

    nightmare begging

---
+++ Assets/LoadGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadGame : MonoBehaviour
+{
+    public void loadrocketmousegame()
+    {
+
+        SceneManager.LoadScene("FinalProject");
+
+
+    }
+
+
+
+
+}
